f9aeecf37069311983640e30245b5229ef110992
Alexey Kalinichenko <alexey.kalinichenko@feedvisor.com>
2020-05-24 14:07:29 +0300

    test 008

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 echo "--------- start entry point--------------"
-
-
 ls
-echo "---------------------------------------"
-ls .
-echo "---------------------------------------"
-ls /root
-echo "---------------------------------------"
+echo "-- node_modules -------------------------------------"
+ls node_modules
+echo "-- node_modules/chromedriver -------------------------------------"
 ls node_modules/chromedriver
-echo "---------------------------------------"
+echo "-- lib/chromedrive-------------------------------------"
 ls node_modules/chromedriver/lib/chromedriver
+echo "-- /root-------------------------------------"
+ls /root
 echo "---------------------------------------"
 
 #chmod +x ./entrypoint.sh
